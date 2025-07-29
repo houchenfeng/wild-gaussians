8295a3d228d613a44e0de35777d8c8c952e3f655
litian <18374328@buaa.edu.cn>
2025-07-30 01:51:48 +0800

    init

---
+++ nvcc.sh
@@ -0,0 +1,3 @@
+export PATH="/usr/local/cuda-12.8/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-12.8/lib64:$LD_LIBRARY_PATH"
+export CUDA_HOME="/usr/local/cuda-12.8/"
---
+++ run.sh
@@ -0,0 +1,3 @@
+#https://blog.csdn.net/nb12138/article/details/145355889 for 
+
+CUDA_VISIBLE_DEVICES=0 nerfbaselines train --method wild-gaussians --data /home/lt/2024/data/dtu_datasets/dtu_robust/scan24/data_0.3
